832bf09220a2e1108143ed3ed8ae6193b1361f03
= <=>
2026-03-03 11:33:04 +0530

    orderstatus and userrole model added, AppDbContext updated

---
+++ Models/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OMS_Backend.Models
+{
+    public class OrderStatus
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; } = string.Empty;
+    }
+}
