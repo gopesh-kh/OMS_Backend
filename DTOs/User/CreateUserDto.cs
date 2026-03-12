94fd78a5d035b156a52a9772655f39363591230c
= <=>
2026-03-12 11:38:52 +0530

    User dto and mapping

---
+++ DTOs/User/CreateUserDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+public class CreateUserDto
+{
+    [Required]
+    [StringLength(50)]
+    public string FirstName { get; set; } = string.Empty;
+
+    [StringLength(50)]
+    public string? LastName { get; set; }
+
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
+}
---
+++ MappingProfiles/UserMappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using OMS_Backend.Models;
+
+namespace OMS_Backend.MappingProfiles
+{
+    public class UserMappingProfile : Profile
+    {
+        public UserMappingProfile() {
+            CreateMap<CreateUserDto, User>()
+                .ForMember(dest => dest.PasswordHash,
+                opt => opt.MapFrom(src => src.Password));
+        }
+    }
+}
